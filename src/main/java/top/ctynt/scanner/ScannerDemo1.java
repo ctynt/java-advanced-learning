46fac739017b5f2b554c2bd9109ff88547a0de63
tyx <2597704619qq.com>
2023-03-22 18:47:06 +0800

    Java Scanner类

---
+++ src/main/java/top/ctynt/scanner/ScannerDemo1.java
@@ -0,0 +1,23 @@
+package top.ctynt.scanner;
+
+import java.util.Scanner;
+
+/**
+ * @Author ctynt
+ * @Date 2023/3/22
+ * @Description next()方法
+ */
+
+public class ScannerDemo1 {
+    public static void main(String[] args) {
+        // 创建扫描器对象
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入一段内容，输入回车结束：");
+        // 可以将用户输入的内容扫描为字符串
+        String str = scanner.next();
+        // 打印输出
+        System.out.println("您输入的内容为：" + str);
+        // 关闭扫描器
+        scanner.close();
+    }
+}
